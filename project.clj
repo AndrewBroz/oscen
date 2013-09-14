4c2ef0df08742f3c185c2e6227a8c340911c8dd6
Andrew Broz <andrew.broz@gmail.com>
2013-09-13 20:55:10 -0500

    added dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject oscen "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+(defproject oscen "0.1.0"
+  :description "music generator"
+  :url "https://github.com/BrazenWord/oscen"
+  :license {:name "WTFPL"
+            :url "http://www.wtfpl.net/txt/copying/"}
+  :dependencies [ [org.clojure/clojure "1.5.1"]
+                  [overtone "0.8.1"] ])
